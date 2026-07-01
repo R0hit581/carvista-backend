6c066573ef53c7ae4db473d91ca06d11e71c1cbe
Rohit Khuswaha <rohitkushwaha581@gmail.com>
2026-07-01 09:52:05 +0530

    Initial commit

---
+++ src/test/java/com/carvista/CarvistaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carvista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarvistaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
